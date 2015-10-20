fcdb90288740122616d08b81f013d8e7ca573d4f
kkane106 <kkane106@gmail.com>
2015-10-20 16:47:51 -0600

    Add layout and index views Add handlebars dependencies to proxy.js

--- proxy.js
+++ proxy.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main';
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
 app.set('port', process.env.PORT || 1337);
 
 app.get('/', function(req, res) {
